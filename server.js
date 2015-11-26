b2898b1d870824ac2b07a6f680daba0c63ab1903
Olga Vyrostko <vyrostkoolga@gmail.com>
2015-11-26 06:59:52 +0500

    fix: server

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
-var http = require('http');
-
-function onRequest(request, response) {
-  // request - http.IncomingMessage
-  // response - http.ServerResponse
-  console.log(request.url);
-  response.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-  response.write('Всем привет !\n');
-  response.end('Hello World !\n');
-}
-
-http.createServer( onRequest ).listen(8000);
-// можно и по шагам:
-// server = http.createServer();
-// server.listen(8000);
-// server.on("request", onRequest);
-
-console.log('Server running at http://127.0.0.1:8000/');
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
