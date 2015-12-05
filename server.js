933c782a20ad975ad97faa2709ebceb19138f900
Olga Vyrostko <vyrostkoolga@gmail.com>
2015-12-05 08:33:53 +0500

    start: серверные скрипты, задача №3

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
-var http = require('http');
-var fs = require('fs');
-var port = process.env.PORT || 1337;
- 
-http.createServer(function (request, response) {
-    fs.readFile('index.html', function (err, data){
-       if (err) {
-       	response.end("File wasn't found");
-       }
-        response.end(data);
-    });
+//Импортируем зависимости
+var express = require( 'express' );
+var bodyParser = require( 'body-parser' );
+var handlebars = require( 'express-handlebars' ).create( );
+var http = require( 'http' );
+
+configure = function( app ) {
+    //Настраиваем bodyParser
+    app.use( bodyParser.json( ) );
+    app.use(bodyParser.urlencoded({
+    extended: true
+    }));
+    
+    //Устанавливаем handlebars в качестве шаблонизатора
+    app.engine('handlebars', handlebars.engine);
+    app.set('view engine', 'handlebars');
+    
+    //Настраиваем доступную для просмотра папку на сервере
+    app.use(express.static(__dirname + '/public'));
+};
+
+var app = express( );
+configure( app );
+
+//Запуск и обработчики возможных запросов
+app.get( '/', function( req, res ) {
+    res.render( 'index' );
+});
+
+
+var port = process.env.port || 8080;
+/*
+http.createServer(function (req, res) {
+    var host = server.address().address,
+        port = server.address().port;
+    console.log('Server running at http://%s:%s', host, port);
 }).listen(port);
+*/
+
+var server = app.listen(port, function () {
+  var host = server.address().address,
+      port = server.address().port;
+  console.log('Server running at http://%s:%s', host, port);
+});
+
 
